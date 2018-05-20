1f2b403a0b017017a6c4424932ffddf2bcb56b48
18chowdhary <chowdharys18@student.westboroughk12.org>
2018-05-20 19:26:22 -0400

    Attempt to get increasing concentration + le chats response working

--- src/lechats/LeChats.java
+++ src/lechats/LeChats.java
@@ -18,14 +18,19 @@ import processing.core.PApplet;
  *   displays in the draw() function because they are cleared each time the seesaw changes position. 
  */
 public class LeChats extends PApplet {
-	 double angle = 1.0;
+	 double angle = 0.0;
 	 ControlP5 gui; 
 	 Button startLeChats; 
+	 Button increaseReactConc; 
+	 Button decreaseReactConc; 
+	 Button increaseProdConc; 
+	 Button decreaseProdConc; 
 	 final int MAX_ANGLE = 20; // due to the geometry of the seesaw, 45 turned out to be way too steep
-	 double q = 105;  
-	 double k = 100; 
-	 float reactantSize = (float) 0.05;
-	 float productSize = (float) 0.05;
+	 double q = 1;  
+	 double k = 1; 
+	 float reactantSize = (float) 0.14;
+	 float productSize = (float) 0.14;
+	 float change = (float) 0.0; 
 	 public static void main(String[] args) {
 	        // TODO Auto-generated method stub
 		 PApplet.main("lechats.LeChats");//required for program to run; calls super of PApplet	
@@ -48,15 +53,16 @@ public class LeChats extends PApplet {
 		startLeChats.addCallback(new CallbackListener() {
 			@Override
 			public void controlEvent(CallbackEvent theEvent) {
-				switch (theEvent.getAction()) {
-					case(ControlP5.ACTION_CLICK): {
+				System.out.println("LE CHATS ACTIVATED");
 						double currAngle = angle; 
 						double initialQ = q;
 						System.out.println("Q - K = "+(initialQ-k));
-						while (Math.abs(q-k)>0.5) {
+						while (Math.abs(q-k)>0.05) {
 							if (q < k) {
-								q+=1;  
+								change = (float) (Math.pow(reactantSize, 2) - Math.pow(productSize, 2))/(2*(productSize + reactantSize));
+								System.out.println("CHANGE: " + change);
 								rotateTo(0);
+								
 							}
 							else if (q > k)
 							{
@@ -72,46 +78,66 @@ public class LeChats extends PApplet {
 							}
 						}
 					}
-				}	
-			}
 			
 		}); 
+		
+		increaseReactConc = gui.addButton("Increase Reactants").setPosition((float)(0.48*width), (float)(0.88*height)).setWidth((int)(0.1*width)).setHeight((int)(0.1*height));
+		increaseReactConc.addCallback(new CallbackListener(){
+			@Override
+			public void controlEvent(CallbackEvent arg0) {
+				float n = (float) 0.001;
+				reactantSize += n;
+			}
+		}); 
+		
+		decreaseReactConc = gui.addButton("Decrease Reactants").setPosition((float)(0.60*width), (float)(0.88*height)).setWidth((int)(0.1*width)).setHeight((int)(0.1*height)); 
+		decreaseReactConc.addCallback(new CallbackListener(){
+
+			@Override
+			public void controlEvent(CallbackEvent arg0) {
+				reactantSize -= 0.001; 
+			}
+			
+		});
 		
 	}
 
 	public void draw() {
+		q = (Math.pow(productSize, 2))/(Math.pow(reactantSize, 2));
 		// Controls the rotation of the seesaw based on q v.s. k
-		if (q < k && Math.abs(q-k)>0.5) rotateCounterClockwise(0.1); 
-		else if (q > k && Math.abs(q-k)>0.5) rotateClockwise(0.1);
-		else if(Math.abs(q-k)<0.5) { rotateTo(0); }  
+		if (q < k && Math.abs(q-k)>0.05) rotateCounterClockwise(0.1); 
+		else if (q > k && Math.abs(q-k)>0.05) rotateClockwise(0.1);
+		else if(Math.abs(q-k)<0.05) { rotateTo(0); } 
 	}
 	
 	public void rotateTo(double goalAngle)
 	{
-		if (angle < goalAngle - 0.05)
+		if (Math.abs(angle-goalAngle)<0.05) 
 		{
-			rotateClockwise(0.1);
+			rotateClockwise(0);
 		}
-		else if (angle > goalAngle + 0.05) 
+		else if (angle > goalAngle) 
 		{
 			rotateCounterClockwise(0.1); 
 		}
-		else if (Math.abs(angle-goalAngle)<0.05)
+		else if (angle < goalAngle)
 		{
-			angle = 0; 
+			rotateClockwise(0.1);
 		}
 	}
 	public void rotateClockwise(double speed)
-	{
+	{	System.out.println("ROTATE CLOCKWISE");
 		System.out.println("Q: " + q);
 		System.out.println("ANGLE: " + angle);
+		System.out.println("REACTANT SIZE: " + reactantSize);
+		System.out.println("PRODUCT SIZE: " + productSize);
 		System.out.println(); 
 		if (speed != 0)
 		{
 			if (angle < MAX_ANGLE) {
 				angle+=speed;
-				if(productSize>=0.03) productSize-=0.001;
-				if(reactantSize<=0.2) reactantSize+=0.001;
+			    productSize += change; 
+			    reactantSize-= change;
 			}
 			background(255); 
 			
@@ -134,20 +160,45 @@ public class LeChats extends PApplet {
 			//reverted back to old system
 			fill(175);
 			triangle((float)(width*0.45), (float)(height*0.61), (float)(width*0.5),(float)(height*0.525), (float)(width*0.55), (float)(height*0.61));
-		}	
+		}
+		else{
+background(255); 
+			
+			//clears the background
+			stroke(0); 
+			//clears the strokes, i think
+			fill(175);
+			//fills the rectangle with this color (dark gray-ish)
+			pushMatrix(); //starts a separate set of transformations; basically creates a new temporary coordinate system
+				translate(width/2, height/2); 
+				//translates the origin of the grid itself to the center of the rect
+				rotate((float) radians((float)angle)); 
+				//rotates the entire grid by the angle
+				rectMode(CENTER); 
+				rect(0,0, (float) (width*0.6), (float) (height*0.05)); 
+				rect((float)(width*-0.2), (float)(height*(-reactantSize/2-0.025)), (float)(width*reactantSize), (float)(height*reactantSize));
+				rect((float)(width*0.2), (float)(height*(-productSize/2-0.025)), (float) (width*productSize), (float)(height*productSize));
+				//since the new origin is the center of the old grid, the new position is (0,0) for the rect
+			popMatrix();
+			//reverted back to old system
+			fill(175);
+			triangle((float)(width*0.45), (float)(height*0.61), (float)(width*0.5),(float)(height*0.525), (float)(width*0.55), (float)(height*0.61));
+		}
 	}
 	
 	public void rotateCounterClockwise(double speed) 
-	{		
+	{	System.out.println("ROTATE COUNTERCLOCKWISE");	
 		System.out.println("Q: " + q);
 		System.out.println("ANGLE: " + angle);
+		System.out.println("REACTANT SIZE: " + reactantSize);
+		System.out.println("PRODUCT SIZE: " + productSize);
 		System.out.println(); 
 		if (speed != 0)
 		{
-			if (angle > -MAX_ANGLE) {
+			if (Math.abs(angle + MAX_ANGLE) > 0.05) {
 				angle-=speed;
-				if(productSize < 0.2) productSize+=0.001;
-				if(reactantSize > 0.03) reactantSize-=0.001;
+			    productSize -= change; 
+			    reactantSize+= change;
 			}
 			background(255); 
 			//clears the background
